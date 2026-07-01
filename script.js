84b9840c555e516fb8465e25bf14cfbca9efc4e9
ytaccc333-hash <ytaccc333@gmail.com>
2026-07-01 22:39:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ function checkCode() {
   const error = document.getElementById("error");
 
   if (code === "JY521") {
-    document.getElementById("loginBox").classList.add("hidden");
-    document.getElementById("mainPage").classList.remove("hidden");
+    document.getElementById("loginBox").style.display = "none";
+    document.getElementById("mainPage").style.display = "block";
   } else {
     error.textContent = "Wrong code!";
   }
+}
+
+function showTab(tabId) {
+  document.querySelectorAll(".tab").forEach(tab => {
+    tab.classList.remove("active");
+  });
+
+  document.getElementById(tabId).classList.add("active");
 }
