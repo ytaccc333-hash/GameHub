55ab25b5b76031bd8cc39968f48b5e6ca38e1ecf
ytaccc333-hash <ytaccc333@gmail.com>
2026-07-01 22:57:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,48 +3,52 @@ function checkCode() {
   const error = document.getElementById("error");
 
   if (code === "JY521") {
-    document.getElementById("loginBox").style.display = "none";
-    document.getElementById("mainPage").style.display = "block";
+    document.getElementById("loginScreen").classList.remove("active");
+    document.getElementById("mainScreen").classList.add("active");
   } else {
     error.textContent = "Wrong code!";
   }
 }
 
 function showTab(tabId) {
-  document.querySelectorAll(".tab").forEach(tab => {
+  document.querySelectorAll("#mainScreen .tab").forEach(tab => {
     tab.classList.remove("active");
   });
-
   document.getElementById(tabId).classList.add("active");
 }
 
-function searchLinks() {
-  let input = document.getElementById("search").value.toLowerCase();
-  let links = document.querySelectorAll("a");
-
-  links.forEach(link => {
-    if (link.innerText.toLowerCase().includes(input)) {
-      link.style.display = "block";
-    } else {
-      link.style.display = "none";
-    }
-  });
-}
-
-/* ADMIN PAGE */
+/* ADMIN */
 function openAdmin() {
   const code = prompt("Enter admin code:");
 
   if (code === "7765") {
-    document.getElementById("mainPage").style.display = "none";
-    document.getElementById("adminPage").style.display = "flex";
+    document.getElementById("mainScreen").classList.remove("active");
+    document.getElementById("adminScreen").classList.add("active");
   } else {
     alert("Wrong admin code");
   }
 }
 
 function closeAdmin() {
-  document.getElementById("adminPage").style.display = "none";
-  document.getElementById("mainPage").style.display = "block";
+  document.getElementById("adminScreen").classList.remove("active");
+  document.getElementById("mainScreen").classList.add("active");
 }
- 
+
+function showAdminTab(tabId) {
+  document.querySelectorAll("#adminScreen .tab").forEach(tab => {
+    tab.classList.remove("active");
+  });
+  document.getElementById(tabId).classList.add("active");
+}
+
+/* SEARCH */
+function searchLinks() {
+  let input = document.getElementById("search").value.toLowerCase();
+  let links = document.querySelectorAll("a");
+
+  links.forEach(link => {
+    link.style.display = link.innerText.toLowerCase().includes(input)
+      ? "block"
+      : "none";
+  });
+}
